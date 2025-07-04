21fe3fa70acff5c33c5b61201d8f38c4cfbf2da9
ekagalang <ekagalangpratama@gmail.com>
2025-07-04 17:18:58 +0700

    Fresh UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,10 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                 'bass-red': '#DA1E1E',
+                 'bass-gold': '#FFD600',
+            },
         },
     },
 
